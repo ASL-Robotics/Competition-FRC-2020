57d763a5801cc558f0298313bd32653dcf4f836c
matteos24 <54120000+matteos24@users.noreply.github.com>
2021-04-06 11:58:52 +0100

    Update BarrelCommandAuto.java

--- CompetitionCode/src/main/java/frc/robot/commands/auto/BarrelCommandAuto.java
+++ CompetitionCode/src/main/java/frc/robot/commands/auto/BarrelCommandAuto.java
@@ -11,14 +11,14 @@ public class BarrelCommandAuto extends SequentialCommandGroup {
         addCommands
         (
             new MoveCommand(drivetrain, 150, MOVE_SPEED), //Move in position to turn for our first obstacle
-            new TurnCommand(drivetrain, 135, TURN_SPEED), //Turn so that we are at a perfectly diagonal slope                                         
+            new TurnCommand(drivetrain, -135, TURN_SPEED), //Turn so that we are at a perfectly diagonal slope                                         
             new MoveCommand(drivetrain, Math.sqrt(150), MOVE_SPEED), //Here we move across two sides of the "square" that we created around the first obstacle
             new TurnCommand(drivetrain, 90, TURN_SPEED),
             new MoveCommand(drivetrain, Math.sqrt(150), MOVE_SPEED),
             new TurnCommand(drivetrain, 90, TURN_SPEED),
               
             new MoveCommand(drivetrain, Math.sqrt(150),  MOVE_SPEED),//Move to the final turn of the square
-            new TurnCommand(drivetrain, 59, TURN_SPEED), //Turn to position ourselves for the second obstacle
+            new TurnCommand(drivetrain, -59, TURN_SPEED), //Turn to position ourselves for the second obstacle
             new MoveCommand(drivetrain, (Math.sqrt(1275)+Math.sqrt(150)),  MOVE_SPEED), //Move all the way to the start of the second obstacle
 
             new TurnCommand(drivetrain, 90, TURN_SPEED), //Here we move across the two sides of the second "square" that we created around the second obstacle
@@ -34,7 +34,7 @@ public class BarrelCommandAuto extends SequentialCommandGroup {
             new TurnCommand(drivetrain, 90, TURN_SPEED),
             new MoveCommand(drivetrain, Math.sqrt(150), MOVE_SPEED), 
 
-            new TurnCommand(drivetrain, -135, TURN_SPEED), //Here we turn to position ourselves so that we can drive to the finish zone
+            new TurnCommand(drivetrain, 135, TURN_SPEED), //Here we turn to position ourselves so that we can drive to the finish zone
             new MoveCommand(drivetrain, 300, MOVE_SPEED) //Here we drive to the finish zone and end this auto
         );
         
